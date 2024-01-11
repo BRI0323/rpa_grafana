00008c8871afd16c367adbd06afadd87d6b94bbd
BRI0323 <bri0323@icloud.com>
2024-01-11 11:38:44 +0900

    change path

--- auto.bat
+++ auto.bat
@@ -1,4 +1,4 @@
 git pull
-cd C:\Users\t-oot\OneDrive\デスクトップ\RPA_grafana-master
+cd C:\Users\t-oot\OneDrive\デスクトップ\RPA_grafana-master\RPA_grafana
 python -m pip install --upgrade pip
 python.exe auto_grafana.py
