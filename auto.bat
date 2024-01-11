918e9ac62acdd73227b24ca4d13b48ab9955bafc
BRI0323 <bri0323@icloud.com>
2024-01-11 11:35:16 +0900

    ANSI

--- auto.bat
+++ auto.bat
@@ -1,3 +1,4 @@
-cd C:\Users\t-oot\OneDrive\デスクトップ\RPA_grafana-master\rpa_grafana
+git pull
+cd C:\Users\t-oot\OneDrive\デスクトップ\RPA_grafana-master
 python -m pip install --upgrade pip
 python.exe auto_grafana.py
